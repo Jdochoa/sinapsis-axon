20f3a37616d257c43079edd2d218e475066377b7
jose.ochoa.reyna@gmail.com <jose.ochoa.reyna@gmail.com@5423394d-3810-039d-a783-85822f7701d9>
2013-02-23 04:19:40 +0000

    Carga Inicial

---
+++ src/Model/trn/sinapsis.axon.m.trn.pas
@@ -0,0 +1,40 @@
+unit sinapsis.axon.m.trn;
+
+
+interface
+
+uses
+  sinapsis.axon.m
+  ,dorm.Mappings
+  ;
+
+type
+  TTrnModel = class(TAxonCatalogo)
+  private
+    { private declarations }
+  protected
+    { protected declarations }
+  public
+    { public declarations }
+
+  published
+    { published declarations }
+  end;
+
+  TCorrModel = class(TAxonCatalogo)
+  private
+    { private declarations }
+  protected
+    { protected declarations }
+  public
+    { public declarations }
+    
+  published
+    { published declarations }
+  end;
+
+
+implementation
+
+
+end.
